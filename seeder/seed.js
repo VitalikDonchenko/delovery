aa5df7acc1d65a88cc400aa84578b487617592f0
Aleksandr <voronamk@mail.ru>
2020-07-22 17:58:14 +0300

    Schemas Fix

--- seeder/seed.js
+++ seeder/seed.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 import UserModel from "../models/userModel.js";
 import CourierModel from "../models/courierModel.js";
-import OrderModel from "../models/orderModel.js";
+import { OfferModel } from "../models/offerModel.js";
+import bcrypt from "bcrypt"
 
 mongoose.pluralize(null);
 mongoose.connect("mongodb://localhost:27017/delovery", {
@@ -15,12 +16,14 @@ mongoose.connection.on(
 
 (async function () {
   const user1 = new UserModel({
-    userName: 'Alex',
-    phone: '+79161234567',
-    email: 'alex@example.com',
-    password: bcrypt.hash('password', 10),
-    location: 'Location',
+    userName: "Alex",
+    phone: "+79161234567",
+    email: "alex@example.com",
+    password: await bcrypt.hash("password", 10),
+    location: "Location",
     ordersArchive: [],
     currentOrder: null,
   });
+  await user1.save();
+  await mongoose.disconnect();
 })();
