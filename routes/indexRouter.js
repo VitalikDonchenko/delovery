da974035095c238a5e7df2fa8de2fcd066d1ca70
Aleksandr <voronamk@mail.ru>
2020-07-23 16:44:55 +0300

    GeoSkeleton

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // res.send('it is alive!')
   res.render('home');
 });
+
+router.post('/geocode', (req, res) => {
+  
+})
 
 export default router;
