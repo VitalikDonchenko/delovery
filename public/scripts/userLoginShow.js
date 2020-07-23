1a510a7ffb841da284784b7a7ef4edd1d8ec8033
Aleksandr <voronamk@mail.ru>
2020-07-23 14:34:29 +0300

    Registration toggle + email skeleton

--- public/scripts/userLoginShow.js
+++ public/scripts/userLoginShow.js
@@ -1,14 +1,21 @@
-const userLoginButton = document.getElementById('userLoginButton');
-const courierLoginButton = document.getElementById('courierLoginButton');
+const userLoginButton = document.getElementById("userLoginButton");
+const courierLoginButton = document.getElementById("courierLoginButton");
+const showLoginButton = document.getElementById("showLoginButton");
 
-userLoginButton.addEventListener('click', (e) => {
+userLoginButton.addEventListener("click", (e) => {
   e.preventDefault();
   e.target.style.display = "none";
-  document.getElementById('userLoginForm').style.display = "block";
-})
+  document.getElementById("userLoginForm").style.display = "block";
+});
 
-courierLoginButton.addEventListener('click', (e) => {
+courierLoginButton.addEventListener("click", (e) => {
   e.preventDefault();
   e.target.style.display = "none";
-  document.getElementById('courierLoginForm').style.display = "block";
-})
+  document.getElementById("courierLoginForm").style.display = "block";
+});
+
+showLoginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  courierLoginButton.style.display = "block";
+  document.getElementById('courierSigninButton').style.display = "block"
+});
