63d99367e7c8ebd73ca6a53336ac1bfb87a6188d
Aleksandr <voronamk@mail.ru>
2020-07-23 11:50:32 +0300

    Added unique true and script for inputs

---
+++ public/scripts/newOffer.js
@@ -0,0 +1,19 @@
+const newInputButton = document.getElementById("goodInput");
+const inputGenerator = (function* () {
+  for (let i = 2; i < 31; i++) {
+    yield `newOffer${i}`;
+  }
+})();
+newInputButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const generatedInput = inputGenerator.next();
+  if(!generatedInput.done) {
+    const newInput =  document.createElement('input');
+    newInput.type = 'text';
+    newInput.name = generatedInput.value;
+    newInput.placeholder='Введите название товара';
+    const container = document.getElementById('offerInputsContainer');
+    container.appendChild(document.createElement('br'));
+    container.appendChild(newInput);
+  }
+});
